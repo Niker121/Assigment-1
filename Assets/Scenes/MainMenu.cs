b06c0565e465f7c2d07dfa052da6e9c884200870
Niker121 <anbatocom3132@gmail.com>
2024-12-02 16:52:03 +0700

    Menu game

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // Để quản lý các cảnh
+using UnityEngine.UI; // Để sử dụng UI
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // Tải cảnh trò chơi (đảm bảo bạn đã thêm cảnh vào Build Settings)
+        SceneManager.LoadScene("Map");
+    }
+    public void ExitGame()
+    {
+        // Thoát ứng dụng
+        Application.Quit();
+        Debug.Log("Game Exited");
+    }
+}
