709857dcf3c34758ab74653d22d64fdcfd9d4ced
Niker121 <anbatocom3132@gmail.com>
2024-12-09 15:35:01 +0700

    Light ui loading

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -1,13 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using Unity.Mathematics;
+using UnityEngine;
 using UnityEngine.SceneManagement; // Để quản lý các cảnh
 using UnityEngine.UI; // Để sử dụng UI
 
 public class MainMenu : MonoBehaviour
 {
-    public void StartGame()
+    public GameObject loeaderUi;
+    public Slider progressSlider;
+
+    public void LoadScene(int index)
     {
-        // Tải cảnh trò chơi (đảm bảo bạn đã thêm cảnh vào Build Settings)
-        SceneManager.LoadScene("Map");
+        StartCoroutine(LoeadScene_Coroutine(index));
+    }
+
+    public IEnumerator LoeadScene_Coroutine(int index)
+    {
+        progressSlider.value = 0;
+        loeaderUi.SetActive(true);
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+        asyncOperation.allowSceneActivation = true;
+        float progress = 0;
+        while (!asyncOperation.isDone)
+        {
+            progress = Mathf.MoveTowards(progress,asyncOperation.progress,Time.deltaTime);
+            progressSlider.value = progress;
+            if( progress >= 0.9f)
+            {
+                progressSlider.value = 1;
+                asyncOperation.allowSceneActivation = true ;
+            }
+            yield return null;
+        }
     }
     public void ExitGame()
     {
